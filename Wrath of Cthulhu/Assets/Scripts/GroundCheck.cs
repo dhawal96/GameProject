dc454a91d3e2f1f44e6375043642140fceb824af
dhawal96 <dsandesara96@gmail.com>
2017-09-22 21:13:46 -0500

    Added character movement mechanics

---
+++ Wrath of Cthulhu/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour {
+
+    private Player player;
+
+    void Start()
+    {
+        player = gameObject.GetComponentInParent<Player>();
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        player.grounded = true;
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        player.grounded = true;
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        player.grounded = false;
+    }
+
+}
---
+++ Wrath of Cthulhu/Assets/Scripts/Player.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float maxSpeed = 3;
+    public float speed = 50f;
+    public float jumpPower;
+    public bool grounded;
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+        jumpPower = 250f;
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        anim.SetBool("Grounded", grounded);
+        anim.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
+
+        if (Input.GetAxis("Horizontal") < -.01f)
+        {
+            transform.localScale = new Vector3(-5.861722f, 5.99821f, 2.873394f);
+        }
+
+        if (Input.GetAxis("Horizontal") > .01f)
+        {
+            transform.localScale = new Vector3(5.861722f, 5.99821f, 2.873394f);
+        }
+
+        if(Input.GetButtonDown("Jump") && grounded)
+        {
+            rb2d.AddForce(Vector2.up * jumpPower);
+            grounded = false;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 easeVelocity = rb2d.velocity;
+        easeVelocity.y = rb2d.velocity.y;
+        easeVelocity.z = 0.0f;
+        easeVelocity.x *= 0.75f;
+
+        float h = Input.GetAxis("Horizontal");
+
+        if (grounded)
+        {
+            rb2d.velocity = easeVelocity;
+        }
+
+        rb2d.AddForce((Vector2.right * speed) * h);
+
+        if (rb2d.velocity.x > maxSpeed)
+        {
+            rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
+        }
+
+        if (rb2d.velocity.x < -maxSpeed)
+        {
+            rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
+        }
+
+    }
+}
