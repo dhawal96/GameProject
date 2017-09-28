d626ae0a30915a9128f9a9015a97dbfc097e94a1
Diego Camacho <diecamacho@Diegos-MacBook-Pro.local>
2017-09-28 16:07:48 -0500

    Added level Design

---
+++ Wrath of Cthulhu/Game Dev/landscape.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="wood_tileset" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="../../../Downloads/wood_tileset.png" width="512" height="512"/>
+</tileset>
