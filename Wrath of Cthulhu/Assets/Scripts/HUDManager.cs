b56aa09e18f8bf38849cdc1e0b54271240e1e6d0
Juan Gutierrez <juangutierrez@myMac.domain.com>
2017-10-03 00:50:25 -0500

    Implemented UI and Main Menu

---
+++ Wrath of Cthulhu/Assets/Scripts/HUDManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDManager : MonoBehaviour {
+
+	private Animator HUDAnim; 
+	// Use this for initialization
+	void Awake () {
+		HUDAnim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	public void HUDFade () {
+		HUDAnim.SetTrigger ("FadeIn");
+	}
+}
---
+++ Wrath of Cthulhu/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour {
+
+	private Animator menuAnim;
+
+	// Use this for initialization
+	void Awake () {
+		menuAnim = GetComponent<Animator> ();
+	}
+	public void MenuFade(){
+		menuAnim.SetTrigger ("FadeOut");
+	}
+	public void LoadScene () {
+		Application.LoadLevel ("PlayerMovement");
+	}
+}
