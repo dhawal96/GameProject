5945ed4873859b13ca37d97c930d065e3c751ebf
Nathan Pham <sythe106@gmail.com>
2017-10-08 22:48:25 -0500

    Juan Gutierrez: Shop Menu added

---
+++ Wrath of Cthulhu/Assets/Scripts/Shop.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour {
+
+	private Animator shopAnim;
+
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void DoneShopping(){
+		Time.timeScale = 1f;
+	}
+
+	// Use this for initialization
+	void Awake () {
+		shopAnim = GetComponent<Animator> ();
+	}
+	public void MenuFade(){
+		shopAnim.SetTrigger ("FadeOut");
+	}
+}
