22704c7ebc5be516ab36dd3b1f8adf759041357c
juandg16 <juandg16@gmail.com>
2017-10-10 15:07:54 -0500

    Main Menu additions

--- Wrath of Cthulhu/Assets/Scripts/Items/ElixirOfLife.cs
+++ Wrath of Cthulhu/Assets/Scripts/Items/ElixirOfLife.cs
@@ -7,4 +7,9 @@ public class ElixirOfLife : MonoBehaviour {
 	public string itemName = "elixir";
 	public int cost = 5;
 	public string description = "Heals the user at the cost of madness.";
+
+	public void SelectItem(int selectIndex) {
+		
+	}
+
 }
--- Wrath of Cthulhu/Assets/Scripts/UI/Shop.cs
+++ Wrath of Cthulhu/Assets/Scripts/UI/Shop.cs
@@ -20,11 +20,50 @@ public class Shop : MonoBehaviour {
 	public Transform ItemTwo;
 	public Transform ItemThree;
 
-	private Vector3 enposition;
+	//Item Text
+	public Transform ItemOneDescription;
+	ShopItemDescription oneDesc;
+	public Transform ItemTwoDescription;
+	ShopItemDescription twoDesc;
+	public Transform ItemThreeDescription;
+	ShopItemDescription threeDesc;
+	public Transform ItemOneName;
+	ShopItemName oneName;
+	public Transform ItemTwoName;
+	ShopItemName twoName;
+	public Transform ItemThreeName;
+	ShopItemName threeName;
+
 
 	// Use this for initialization
 	void Start () {
-		GameObject itemOneObject = Instantiate(items[0], ItemOne.position, ItemOne.rotation) as GameObject;
+		//Update and handle item descriptions
+		ItemOneDescription = GameObject.Find ("ItemOneDescription").transform;
+		ItemTwoDescription = GameObject.Find ("ItemTwoDescription").transform;
+		ItemThreeDescription = GameObject.Find ("ItemThreeDescription").transform;
+		oneDesc = ItemOneDescription.GetComponent<ShopItemDescription> ();
+		oneDesc.cost = items [0].GetComponent<ElixirOfLife> ().cost;
+		oneDesc.description = items [0].GetComponent<ElixirOfLife> ().description;
+		twoDesc = ItemTwoDescription.GetComponent<ShopItemDescription> ();
+		twoDesc.cost = items [1].GetComponent<EyeOfAzethoth> ().cost;
+		twoDesc.description = items [1].GetComponent<EyeOfAzethoth> ().description;
+		threeDesc = ItemThreeDescription.GetComponent<ShopItemDescription> ();
+		threeDesc.cost = items [2].GetComponent<Morphine> ().cost;
+		threeDesc.description = items [2].GetComponent<Morphine> ().description;
+
+		//Update and handle item name
+		ItemOneName = GameObject.Find ("ItemOneName").transform;
+		ItemTwoName = GameObject.Find ("ItemTwoName").transform;
+		ItemThreeName = GameObject.Find ("ItemThreeName").transform;
+		oneName = ItemOneName.GetComponent<ShopItemName> ();
+		oneName.name = "Elixir of Life";
+		twoName = ItemTwoName.GetComponent<ShopItemName> ();
+		twoName.name = "Eye of Azathoth";
+		threeName = ItemThreeName.GetComponent<ShopItemName> ();
+		threeName.name = "Morphine";
+
+		//Create Items
+		GameObject itemOneObject = Instantiate(items[0], ItemOne.position+(transform.forward*-2), ItemOne.rotation) as GameObject;
 		itemOneObject.transform.SetParent (transform, true);
 		itemOneObject.GetComponent<RectTransform> ().localScale = new Vector3 (1, 1, 1);
 		GameObject itemTwoObject = Instantiate(items[1], ItemTwo.position, ItemTwo.rotation) as GameObject;
@@ -33,23 +72,11 @@ public class Shop : MonoBehaviour {
 		GameObject itemThreeObject = Instantiate(items[2], ItemThree.position, ItemThree.rotation) as GameObject;
 		itemThreeObject.transform.SetParent (transform, true);
 		itemThreeObject.GetComponent<RectTransform> ().localScale = new Vector3 (1, 1, 1);
+
+
 		Time.timeScale = 0f;
-		/*Player = GameObject.Find ("Player").transform;
+		Player = GameObject.Find ("Mark").transform;
 		playerscript = Player.GetComponent <Player> ();
-
-
-		enposition = gameObject.GetComponent<EnemyMove>().transform.position;
-		/*if (randomIndex <= 1f && randomIndex >= 1f)
-        {
-            //itemIndex = Random.Range(0, collision.gameObject.GetComponent<EnemyMove>().items.Length - 1);
-            GameObject coin = Instantiate(gameObject.GetComponent<EnemyMove>().items[3], enposition, Quaternion.identity);
-            Destroy(coin, 5);
-        }
-
-        else
-        {
-		current = Random.Range(0, gameObject.GetComponent<EnemyMove>().items.Length - 1);
-		Instantiate(gameObject.GetComponent<EnemyMove>().items[current], enposition, Quaternion.identity);*/
 	}
 	
 	// Update is called once per frame
@@ -69,7 +96,16 @@ public class Shop : MonoBehaviour {
 		shopAnim.SetTrigger ("FadeOut");
 	}
 
-	void SelectItem(int selectIndex) {
-		playerscript.item = items [selectIndex].name;
+	public void SelectItem(int selectIndex) {
+		if (selectIndex == 0) {
+			playerscript.item = "elixir";
+		}
+		else if (selectIndex == 1) {
+			playerscript.item = "blink";
+		}
+		else if (selectIndex == 2) {
+			playerscript.item = "morphine";
+		}
+		//playerscript.item = items [selectIndex].itemName;
 	}
 }
