f2f877db04595a7701126ca482b770b2cf4fb9ae
dhawal96 <dsandesara96@gmail.com>
2017-10-22 17:43:23 -0500

    Changed Enemy Attack Detection from Collision to Trigger

---
+++ Wrath of Cthulhu/Assets/Scripts/enemyAttackTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyAttackTrigger : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            transform.parent.gameObject.GetComponent<EnemyMove>().Damage();
+        }
+    }
+}
