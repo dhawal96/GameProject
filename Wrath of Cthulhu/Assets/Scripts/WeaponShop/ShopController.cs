f1ea01f84773ea69721cd5182e1d37402ed61b3b
dhawal96 <dsandesara96@gmail.com>
2017-10-27 20:53:28 -0500

    Fully Functional Item Upgrade Shop

---
+++ Wrath of Cthulhu/Assets/Scripts/WeaponShop/ShopController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopController : MonoBehaviour {
+
+    public GameObject shopPanel;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            OpenShop();
+        }
+
+    }
+
+    void OpenShop()
+    {
+        shopPanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void CloseShop()
+    {
+        shopPanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
---
+++ Wrath of Cthulhu/Assets/Scripts/WeaponShop/WeaponObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class WeaponObject : ScriptableObject
+{
+
+    public string weaponName = "Weapon Name Here";
+    public string itemCode = "itemCode";
+    public int cost = 5;
+    public string description;
+
+
+}
